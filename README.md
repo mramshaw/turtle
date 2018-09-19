745a69fb5c0ed6716f9b87686ace7293c952dae0
mramshaw <mramshaw@alumni.concordia.ca>
2018-09-19 10:41:23 -0400

    Add progress logging and 12-Factor port

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 # turtle
-A simple app that returns delayed results. The intent of this application is that sometimes when you're writing UI tests, you might have a test that timesout and doesn't wait long enough. To specifically test these kinds of tests, I wrote an application that is specifically slow so that you can test timeout issues.
 
-# To Run
+A simple Golang web app that returns delayed results.
+
+## Motivation
+
+The intent of this application is that sometimes when you're writing tests,
+you might have a test that timesout and doesn't wait long enough. To specifically
+test these kinds of tests, this application is specifically slow so that you can
+test timeout issues.
+
+[I wrote a PHP web page specifically for this issue, but nice to have a native
+ Golang version as well.]
+
+## To Run
+
 This is a golang application. You can run the app with:
+
 ```
-go install
-go run main.go
+$ PORT=8070 go run main.go
 ```
 
+## To Do
+
+- [x] Allow `port` to be specified in a 12-Factor way
+- [x] Refactor code to conform to `golint` suggestions
+- [x] Run `go fmt` to clean up code formatting
+- [ ] Allow timeout to be specified
--- main.go
+++ main.go
@@ -1,64 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "math/rand"
-    "net/http"
-    "text/template"
-    "time"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
+// Page represents the web page
 type Page struct {
-    Speed int32
+	Speed int32
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
-    router.HandleFunc("/login", login)
+	port := os.Getenv("PORT")
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+	router.HandleFunc("/todos", todoIndex)
+	router.HandleFunc("/todos/{todoID}", todoShow)
+	router.HandleFunc("/login", login)
+
+	log.Printf("Now listening on http://localhost:%v ...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    speed := turtleMe()
-    page := &Page{Speed: speed}
+	speed := turtleMe()
+	page := &Page{Speed: speed}
 
-    fmt.Println("method:", r.Method) //get request method
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("./resources/sample.html")
-        t.Execute(w, page)
-    } else {
-        r.ParseForm()
-        // logic part of log in
-        fmt.Println("username:", r.Form["username"])
-        fmt.Println("password:", r.Form["password"])
-    }
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("./resources/sample.html")
+		t.Execute(w, page)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
 
 func turtleMe() int32 {
-    yourTime := rand.Int31n(10)
-    time.Sleep(time.Duration(yourTime) * time.Second)
-    return yourTime
+	yourTime := rand.Int31n(10)
+	time.Sleep(time.Duration(yourTime) * time.Second)
+	return yourTime
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-    yourTime := turtleMe()
-    fmt.Fprintf(w, "Took %v to get to the result", yourTime)
-    fmt.Fprintln(w, "Welcome!")
+func index(w http.ResponseWriter, r *http.Request) {
+	yourTime := turtleMe()
+	fmt.Fprintf(w, "It took %v second(s) to get the result.\n\n", yourTime)
+	fmt.Fprintln(w, "Welcome!")
 }
 
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Todo Index!")
+func todoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
 }
 
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
+func todoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoID"]
+	fmt.Fprintln(w, "Todo show:", todoID)
 }
